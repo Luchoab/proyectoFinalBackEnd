0eaf35f655f8d57d821ea7fbf8f6bac0c6661703
Luciano Abraham <lucianoabrahammoise27@gmail.com>
2023-01-06 21:39:43 -0300

    Se instalo nodemon, mongoose y axios

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-//Instalar express js para que escuche constantemente el servidor
-const { json } = require('express');
+//Configurar servidor express para que siempre este escuchando.
 const express = require('express');
+const mongoose  = require('mongoose');
 const app = express();
 const port = 3001;
-const axios = require('axios').default
 
-app.get('/', (req,res) => {
+//Vinculasmos la base de datos.
+const password = 'SWh5DubdRvNI6nUh'
+const URL = `mongodb+srv://proyectoFinal:${password}@proyectofinlabackend.ipvbroy.mongodb.net/test`;
 
-});
+//Llamamos al schema de usuarios
+const User = require('./schemas/user.schema')
 
-app.listen(port, ()=>{
-    try {
-        console.log(`Escuchando desde el puerto ${port}`);
-    } catch (error) {
-        console.log('Error al levantar el servidor');
-    }
-    
-});
+
+
+//Nos conectamos a la base de dato con mongoose y funcion async.
+async function main(){
+    console.log('\x1b[34m Conectando a la base de datos...\x1b[37m');
+    mongoose.set('strictQuery', false)
+    await mongoose.connect(URL)
+    console.log('\x1b[95m Se ha conectado correctamente a la base de datos \x1b[37m');
+
+    app.listen(port, ()=>{
+        try {
+            console.log(`\x1b[32m Escuchando desde el puerto ${port} \x1b[37m`);
+        } catch (error) {
+            console.log('\x1b[31 Error al levantar el servidor \x1b[34');
+        }
+    });
+}
+
+main().catch(error => console.error('\x1b[31m Error al conectar a la base de datos \x1b[37m',error));
+
+
+// Llamamos al edpoint con un metodo (get,post,delete,etc) y funciones que queremos ejectuar.
+app.get('/', (req,res)=>{
+    res.send('Hola desde el home')
+})
+
